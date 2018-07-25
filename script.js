b2a37a52c9a682d80e25819645a258609f93afdf
dyuki <unblivion@gmail.com>
2018-07-25 09:06:59 -0300

    Acrescentando JS, 50%

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-const 
+const casas = document.querySelectorAll(".casa");
+let trocar = 0;
+/*
+for(let casa in casas){
+    casas[casa].onclick = clicar;
+}
+*/
+
+for(let casa of casas){
+    casa.onclick = clicar;
+}
+
+function clicar(){
+    if (trocar % 2){
+        this.innerHTML = "X";
+        this.style.color = "blue";
+        trocar++;
+    }
+    else{
+        this.innerHTML = "O";
+        this.style.color = "red";
+        trocar++;
+    }
+}
